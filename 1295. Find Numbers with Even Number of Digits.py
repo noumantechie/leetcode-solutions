370baa8eb6c17406053f58e3399275a79a952388
Nomanyousaf07 <nomanyousaf712@gmail.com>
2025-02-05 23:02:08 +0500

    square of sorted array

--- 1295. Find Numbers with Even Number of Digits.py
+++ 1295. Find Numbers with Even Number of Digits.py
@@ -1,9 +1,30 @@
-
+# First Approach
+#  --------------------------------------Using Direct Integer Division--------------------------------------------
 nums = [12, 34, 2, 6, 7896]
 
-count = 0
 
+def count_digits(n):
+    digits = 0
+    while n > 0:
+        n = n // 10
+        digits += 1
+    return digits
+
+
+count = 0
 for num in nums:
-    if len(str(num)) % 2 == 0:
-        count = count + 1
+    if count_digits(num) % 2 == 0:
+        count += 1
+
 print(count)
+
+
+# Second Approach
+# -------------------------------------Using String Conversion ----------------------------------------------
+
+# count = 0
+
+# for num in nums:
+#     if len(str(num)) % 2 == 0:
+#         count = count + 1
+# print(count)
---
+++ 977.Squares of a Sorted Array.py
@@ -0,0 +1,10 @@
+
+nums = [-4, -1, 0, 3, 10]
+
+squared_list = []
+for num in nums:
+    squared_list.append(num ** 2)
+
+sorted_list = sorted(squared_list)  # Sort the squared numbers
+
+print(sorted_list)
